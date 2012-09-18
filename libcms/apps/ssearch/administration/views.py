f99c40cc1339b73d1eb44877fc693f19d3022699
Sergey <dostovalov@gmail.com>
2012-09-18 18:49:53 +0400

    sserach

--- libcms/apps/ssearch/administration/views.py
+++ libcms/apps/ssearch/administration/views.py
@@ -396,7 +396,7 @@ def _indexing(slug, reset=False):
 
         docs.append(doc)
         i+=1
-        if len(docs) > 300:
+        if len(docs) > 50:
             print 'add ' + str(len(docs))
             solr.add(docs)
             print i
