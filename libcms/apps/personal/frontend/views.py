4d5ce190716f0e3035be9f0c1b4408075dc10d6c
Sergey <dostovalov@gmail.com>
2012-10-01 18:12:27 +0400

    accounts fix

--- libcms/apps/personal/frontend/views.py
+++ libcms/apps/personal/frontend/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def index(request):
     return render(request, 'personal/frontend/index.html')
