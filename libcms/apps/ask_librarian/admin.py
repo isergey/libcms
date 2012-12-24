e6f30dbe74549f6e4bf87a6e07a5e710f308a595
Sergey <dostovalov@gmail.com>
2012-12-24 14:13:48 +0400

    ask lib import

--- libcms/apps/ask_librarian/admin.py
+++ libcms/apps/ask_librarian/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
 
-from models import QuestionManager
+from models import QuestionManager, Question
 
 
 class QuestionManagerAdmin(admin.ModelAdmin):
@@ -10,4 +10,9 @@ class QuestionManagerAdmin(admin.ModelAdmin):
 admin.site.register(QuestionManager,QuestionManagerAdmin)
 
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question', 'create_date')
+
+admin.site.register(Question,QuestionAdmin)
 
