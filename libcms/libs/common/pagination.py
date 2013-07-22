6c0d206b01bb0589b1099fdfc10c08d1a1dde4c2
Sergey <you@example.com>
2013-07-22 17:00:48 +0400

    pagination fix

--- libcms/libs/common/pagination.py
+++ libcms/libs/common/pagination.py
@@ -8,15 +8,18 @@ def get_page(request, objects_qs, per_page=20):
     per_page - количество извлекаемых объектов на страницу
     """
     paginator = Paginator(objects_qs, per_page)
-    page = request.GET.get('page', 1)
+    try:
+        page = request.GET.get('page', 1)
+    except ValueError:
+        page = 1
 
-    if page == '0': page = 1
-    if page > str(paginator.num_pages): page = paginator.num_pages
+    if page == 0:
+        page = 1
+
+    if page > paginator.num_pages:
+        page = paginator.num_pages
     try:
         objects = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        objects = paginator.page(1)
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
         objects = paginator.page(paginator.num_pages)
