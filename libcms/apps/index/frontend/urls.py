992c69c1e2a8157ef9b99cd214c7f3e4b9b80776
Sergey Dostovalov <dostovalov@gmail.com>
2014-11-12 18:02:36 +0300

    bib sites

--- libcms/apps/index/frontend/urls.py
+++ libcms/apps/index/frontend/urls.py
@@ -4,4 +4,7 @@ import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name="index"),
     url(r'^site/$', views.site, name="site"),
+    url(r'^site/news/$', views.site_news_list, name="site_news_list"),
+    url(r'^site/news/(?P<id>\d+)/$', views.site_news_detail, name="site_news_detail"),
+    url(r'^site/about/$', views.site_page, name="site_page"),
 )
--- libcms/apps/index/frontend/views.py
+++ libcms/apps/index/frontend/views.py
@@ -6,3 +6,12 @@ def index(request):
 
 def site(request):
     return render(request, 'index/frontend/org_site.html')
+
+def site_page(request):
+    return render(request, 'index/frontend/org_page.html')
+
+def site_news_list(request):
+    return render(request, 'index/frontend/org_news_list.html')
+
+def site_news_detail(request, id):
+    return render(request, 'index/frontend/org_news_detail.html')
