7311db9dff1bbc16bc11bfdcc68f8dcf986e24ab
Sergey Dostovalov <dostovalov@gmail.com>
2015-04-21 19:53:12 +0300

    corshead remove

--- libcms/settings.py
+++ libcms/settings.py
@@ -88,7 +88,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django_sorting.middleware.SortingMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     # 'statistics.middleware.RequestLog',
     # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
