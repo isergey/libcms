7df7646af9f6abbff53a46d4e7469bad86c3a6e6
Sergey Dostovalov <dostovalov@gmail.com>
2015-04-22 15:20:09 +0300

    remove branch

--- libcms/apps/participants/models.py
+++ libcms/apps/participants/models.py
@@ -121,7 +121,7 @@ class UserLibrary(models.Model):
     library = models.ForeignKey(Library)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     middle_name = models.CharField(verbose_name=u'Отчество', max_length=255, blank=True)
-    branch = models.CharField(verbose_name=u'Отдел', max_length=255)
+    # branch = models.CharField(verbose_name=u'Отдел', max_length=255)
     position = models.ForeignKey(UserLibraryPosition, verbose_name=u'Должность', null=True, blank=True)
     phone = models.CharField(verbose_name=u'Телефон', max_length=32)
     is_active = models.BooleanField(
