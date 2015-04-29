1cd26929c1c2811e1dbdef3bdaf18ef1ade8d15a
Sergey Dostovalov <dostovalov@gmail.com>
2015-04-29 20:05:17 +0300

    banners

--- libcms/apps/participant_banners/models.py
+++ libcms/apps/participant_banners/models.py
@@ -5,16 +5,17 @@ from participants.models import Library
 
 
 class Banner(models.Model):
-    libraries = models.ManyToManyField(Library, blank=True)
+    libraries = models.ManyToManyField(Library, blank=True, verbose_name=u'Организации, на сайтах которых будет показан банер')
     image = models.ImageField(upload_to=u'participant_banners/%Y/%m/%d')
-    title = models.CharField(max_length=500, blank=True)
-    description = models.TextField(max_length=1000, blank=True)
+    title = models.CharField(max_length=500, blank=True, verbose_name=u'Название')
+    description = models.TextField(max_length=1000, blank=True, verbose_name=u'Описание')
     url = models.CharField(max_length=500, verbose_name=u'Ссылка для клика по банеру', blank=True)
     target_blank = models.BooleanField(default=False, verbose_name=u'Открывать ссылку в новой вкладке')
     show_period = models.IntegerField(default=5, help_text=u'Период показа в секундах')
     global_banner = models.BooleanField(
         default=False,
         db_index=True,
+        verbose_name=u'Глобальный банер',
         help_text=u'Будет отображен на всех сайтах без исключения'
     )
     owned = models.BooleanField(
@@ -22,7 +23,20 @@ class Banner(models.Model):
         verbose_name=u'Банером может управлять организация',
         help_text=u'Возможность отсутствует, если банер был создан вышестоящим администратором'
     )
-    active = models.BooleanField(default=True, db_index=True)
+    active = models.BooleanField(
+        default=True,
+        db_index=True,
+        verbose_name=u'Активный'
+    )
+    order = models.IntegerField(
+        default=0,
+        verbose_name=u'Порядок вывода',
+        db_index=True
+    )
     start_date = models.DateTimeField(verbose_name=u'Дата начала показа', db_index=True)
     end_date = models.DateTimeField(verbose_name=u'Дата окончания показа', db_index=True)
 
+    class Meta:
+        ordering = ['-order', '-id']
+        verbose_name = u'Банер'
+        verbose_name_plural = u'Банеры'
