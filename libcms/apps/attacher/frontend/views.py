5edf9f3bc04cf7e4549c5e0ea1a72bcac8e2b75a
Sergey Dostovalov <dostovalov@gmail.com>
2015-05-20 12:22:50 +0300

    participants

--- libcms/apps/attacher/frontend/views.py
+++ libcms/apps/attacher/frontend/views.py
@@ -1,15 +1,15 @@
 # encoding: utf-8
+import sys
 import shutil
 import json
 import uuid
-from datetime import datetime
 import os
 import hashlib
 from django.conf import settings
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import HttpResponse, render
 from django.views.decorators.csrf import csrf_exempt
 
+FILE_SYSTEM_ENCODING = getattr(settings, 'FILE_SYSTEM_ENCODING', 'utf-8')
 
 ATTACH_PREFIX = 'attaches/'
 TMP_PREFIX = 'tmp/'
@@ -22,7 +22,6 @@ ATTACH_TMP_URL = ATTACH_BASE_URL + TMP_PREFIX
 
 
 @csrf_exempt
-@login_required
 def index(request):
     if request.method == 'POST':
         content_type = request.GET.get('content_type')
@@ -33,7 +32,6 @@ def index(request):
     })
 
 
-@login_required
 def upload(request, content_type, content_id):
     upload_info = handle_uploaded_file(request.FILES.get('files[]'), content_type, content_id)
     return HttpResponse(json.dumps({
@@ -44,7 +42,6 @@ def upload(request, content_type, content_id):
     }))
 
 
-@login_required
 def delete(request):
     path = request.GET.get('path', None)
     sign = request.GET.get('sign', None)
@@ -56,7 +53,7 @@ def delete(request):
 
     if sign != get_sign_delete_path(path):
         return HttpResponse(u'Wrong path', status=403)
-    target_file_path = get_dir_for_object(content_type, content_id) + path
+    target_file_path = (get_dir_for_object(content_type, content_id) + path).encode(FILE_SYSTEM_ENCODING)
     if os.path.isfile(target_file_path):
         os.remove(target_file_path)
 
@@ -88,7 +85,7 @@ def handle_uploaded_file(f, content_type, content_id):
     if not os.path.isdir(target_dir_path):
         os.makedirs(target_dir_path, 0775)
 
-    with open(target_dir_path + file_name, 'wb+') as destination:
+    with open(os.path.join(target_dir_path, file_name).encode(FILE_SYSTEM_ENCODING), 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
 
@@ -100,7 +97,7 @@ def handle_uploaded_file(f, content_type, content_id):
 
 
 def get_sign_delete_path(delete_path):
-    return hashlib.md5(unicode(delete_path).encode('utf-8') + settings.SECRET_KEY).hexdigest()
+    return hashlib.md5(unicode(delete_path).encode(FILE_SYSTEM_ENCODING) + settings.SECRET_KEY).hexdigest()
 
 
 def get_dir_for_object(content_type, content_id):
@@ -116,6 +113,6 @@ def get_media_url_for_file(content_type, content_id, file_name):
 
 
 def delete_content_attaches(content_type, content_id):
-    content_dir = get_dir_for_object(content_type, content_id)
+    content_dir = get_dir_for_object(content_type, content_id).encode(FILE_SYSTEM_ENCODING)
     if os.path.isdir(content_dir):
         shutil.rmtree(content_dir)
