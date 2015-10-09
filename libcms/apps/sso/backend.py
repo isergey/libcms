723a6e01a269ac2b72dcd296bf82a32ae1d267ba
Sergey Dostovalov <dostovalov@gmail.com>
2015-10-09 16:12:14 +0300

    sso esia

--- libcms/apps/sso/backend.py
+++ libcms/apps/sso/backend.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class SSOBackend(object):
     def authenticate(self, user_model):
         try:
-            return User.objects.get(pk=user_model)
+            return User.objects.get(pk=user_model.pk)
         except User.DoesNotExist:
             return None
 
