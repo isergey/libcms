a6f6512555c6736a2fafca4d7cd6c0279a0939b3
Sergey Dostovalov <dostovalov@gmail.com>
2016-11-14 15:37:46 +0300

    sso tatedu

--- libcms/apps/sso_tatedu/models.py
+++ libcms/apps/sso_tatedu/models.py
@@ -2,33 +2,33 @@ import json
 from django.db import models
 
 
-class EsiaUser(models.Model):
+class TatEduUser(models.Model):
     oid = models.CharField(max_length=32, db_index=True, unique=True)
     user_attrs = models.TextField(max_length=101024)
     create_data = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
 
-def create_or_update_esia_user(oid, user_attrs):
+def create_or_update_user(oid, user_attrs):
     user_attrs_json = user_attrs
 
     if isinstance(user_attrs, object):
         user_attrs_json = json.dumps(user_attrs, ensure_ascii=False, encoding='utf-8')
 
     try:
-        esia_user = EsiaUser.objects.get(oid=oid)
-    except EsiaUser.DoesNotExist:
-        esia_user = EsiaUser(oid=oid, user_attrs=user_attrs_json)
-        esia_user.save()
-        return esia_user
+        user = TatEduUser.objects.get(oid=oid)
+    except TatEduUser.DoesNotExist:
+        user = TatEduUser(oid=oid, user_attrs=user_attrs_json)
+        user.save()
+        return user
 
     need_update = False
 
-    if esia_user.user_attrs != user_attrs_json:
-        esia_user.user_attrs = user_attrs_json
+    if user.user_attrs != user_attrs_json:
+        user.user_attrs = user_attrs_json
         need_update = True
 
     if need_update:
-        esia_user.save()
+        user.save()
 
-    return esia_user
+    return user
