35675dc9b19f9dd6fd8d497959548304b004f71d
Sergey Dostovalov <dostovalov@gmail.com>
2016-12-06 14:11:04 +0300

    statistics

--- libcms/apps/statistics/api/urls.py
+++ libcms/apps/statistics/api/urls.py
@@ -2,9 +2,10 @@
 from django.conf.urls import *
 import views
 
-urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index'),
-                       url(r'^org/$', views.org_stats, name='org_stats'),
-                       url(r'^search/$', views.search_stats, name='search_stats'),
-                       url(r'^watch/$', views.watch, name='watch'),
-                       )
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.index, name='index'),
+    url(r'^org/$', views.org_stats, name='org_stats'),
+    url(r'^search/$', views.search_stats, name='search_stats'),
+    url(r'^watch/$', views.watch, name='watch'),
+)
