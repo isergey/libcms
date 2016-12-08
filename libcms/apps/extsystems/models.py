403f3d08aa02f574cb253f723f138f26c5c2764b
Sergey Dostovalov <dostovalov@gmail.com>
2016-12-08 15:15:29 +0300

    extsystems

--- libcms/apps/extsystems/models.py
+++ libcms/apps/extsystems/models.py
@@ -2,19 +2,29 @@
 import uuid
 from django.db import models
 
+
 def generate_uuid():
-   return uuid.uuid4()
+    return uuid.uuid4()
+
+
+STATUS_CHOICES = (
+    (False, u'неактивно'),
+    (True, u'активно'),
+)
+
 
 class Party(models.Model):
+    name = models.CharField(
+        verbose_name=u'Название',
+        max_length=255
+    )
     contactemail = models.EmailField(
         verbose_name=u'Контатный email',
         max_length=255,
-        blank=True,
-        null=True
     )
     contactperson = models.CharField(
         verbose_name=u'Контактное лицо',
-        max_length=255, blank=True, null=True
+        max_length=255
     )
     endpoint = models.CharField(
         verbose_name=u'Точка доступа',
@@ -28,13 +38,10 @@ class Party(models.Model):
         verbose_name=u'Минуты',
         blank=True, null=True
     )
-    name = models.CharField(
-        verbose_name=u'Название',
-        max_length=255, blank=True, null=True
-    )
-    notify = models.NullBooleanField()
-    status = models.NullBooleanField()
-    token = models.CharField(max_length=255, blank=True, null=True, default=generate_uuid)
+
+    notify = models.BooleanField(verbose_name=u'Уведомлять')
+    status = models.BooleanField(verbose_name=u'Активно', default=False)
+    token = models.CharField(max_length=255, blank=True, default=generate_uuid)
 
     class Meta:
         managed = False
