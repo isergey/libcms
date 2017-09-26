c6ac84a5ece2c82baeb8150d8dfaf2eb1a0e5c37
Sergey Dostovalov <dostovalov@gmail.com>
2017-09-26 18:57:03 +0300

    esia

--- libcms/apps/sso_tatedu/views.py
+++ libcms/apps/sso_tatedu/views.py
@@ -65,20 +65,129 @@ def _generate_password(length=PASSWORD_LENGTH):
     return "".join([chars[ord(c) % len(chars)] for c in os.urandom(length)])
 
 
-def _update_ruslan_user(grs_user_record):
-    portal_client = connection_pool.get_client(RUSLAN_API_ADDRESS, RUSLAN_API_USERNAME, RUSLAN_API_PASSWORD)
+# def _generate_registraion_address(person_address):
+#     registration_address_parts = []
+#
+#     def add_to_registration_address(item):
+#         if not item:
+#             return
+#         registration_address_parts.append(item.strip())
+#
+#     add_to_registration_address(person_address.get('region', ''))
+#     add_to_registration_address(person_address.get('city', ''))
+#     add_to_registration_address(person_address.get('street', ''))
+#     add_to_registration_address(person_address.get('house', ''))
+#
+#     flat = person_address.get('flat', '')
+#     if flat:
+#         flat = u'кв. ' + flat
+#         add_to_registration_address(flat)
+#
+#     return ', '.join(registration_address_parts)
 
-    fields_1 = grs_user_record.get_field('1')
-    record_id = ''
+#
+# def _generate_region_city(person_address):
+#     region_city_parts = []
+#     region = person_address.get('region', '')
+#     city = person_address.get('city', '')
+#
+#     if region:
+#         region_city_parts.append(region)
+#     if city:
+#         region_city_parts.append(city)
+#     return ' / '.join(region_city_parts)
 
-    if fields_1:
-        record_id = fields_1[0].content
 
-    if not record_id:
-        raise ValueError('record_id must be not empty')
+def _create_grs_from_user(oid, email='', user_attrs=None):
+    user_attrs = user_attrs or {}
+    gender_map = {
+        'male': u'М',
+        'female': u'Ж'
+    }
 
-    portal_client.update_grs(grs_record=grs_user_record, database=RUSLAN_USERS_DATABASE, id=record_id)
-    return grs_user_record
+    person_info = user_attrs.get('person_info', {})
+    # person_contacts = user_attrs.get('person_contacts', {})
+    # person_addresses = user_attrs.get('person_addresses', [])
+    # rf_passports = filter(lambda doc: doc['type'] == 'RF_PASSPORT' and 'Identifiable' in doc['stateFacts'],
+    #                       user_attrs.get('person_docs', []))
+    # reg_address = {}
+    # prozhiv_address = {}
+    #
+    # for person_address in person_addresses:
+    #     if person_address.get('type', '') == 'PRG':
+    #         reg_address = person_address
+    #     elif person_address.get('type', '') == 'PLV':
+    #         prozhiv_address = person_address
+
+    # phone_contact = {}
+    # for person_contact in person_contacts:
+    #     if person_contact.get('type', '') == 'MBT':
+    #         phone_contact = person_contact
+
+    # foreigner = u''
+    # citizenship = person_info.get('citizenship', '').lower()
+    #
+    # if citizenship:
+    #     if citizenship == 'rus':
+    #         foreigner = u'0'
+    #     else:
+    #         foreigner = u'1'
+
+    birth_date = person_info.get('birth_year', '')
+    if birth_date:
+        birth_date += '0101'
+
+    record = grs.Record()
+
+    def add_field_to_record(tag, value):
+        if not value:
+            return
+        record.add_field(grs.Field(tag, value))
+
+    add_field_to_record('101', normalize_fio(person_info.get('lname', '')))
+    add_field_to_record('102', normalize_fio(person_info.get('fname', '')))
+    add_field_to_record('103', normalize_fio(person_info.get('pname', '')))
+    add_field_to_record('105', datetime.now().strftime('%Y%m%d'))
+    add_field_to_record('115', _generate_password())
+    # add_field_to_record('120', phone_contact.get('value', ''))
+    # add_field_to_record('121', _generate_registraion_address(prozhiv_address))
+    add_field_to_record('122', email)
+    # add_field_to_record('130', _generate_registraion_address(reg_address))
+    add_field_to_record('234', birth_date)
+    # add_field_to_record('402', person_info.get('snils', ''))
+    add_field_to_record(OID_FIELD, oid)
+    add_field_to_record('404', gender_map.get(person_info.get('gender', '').lower(), u''))
+
+    organisations = person_info.get('organizations', [])
+    for organisation in organisations:
+        add_field_to_record('505', unicode(organisation.get('id', '')))
+        add_field_to_record('506', unicode(organisation.get('region', {}).get('id')))
+    # if rf_passports:
+    #     rf_passport = rf_passports[-1]
+    #     add_field_to_record('417', u'Паспорт')
+    #     add_field_to_record('418', rf_passport.get('series', ''))
+    #     add_field_to_record('419', rf_passport.get('number', ''))
+    #     add_field_to_record('420', rf_passport.get('issuedBy', ''))
+    #     issue_date_parts = rf_passport.get('issueDate', '').split('.')
+    #     issue_date = ''
+    #     if len(issue_date_parts) == 3:
+    #         issue_date = issue_date_parts[2] + issue_date_parts[1] + issue_date_parts[0]
+    #     add_field_to_record('421', issue_date)
+    #     add_field_to_record('422', rf_passport.get('issueId', ''))
+    #
+    # add_field_to_record('423', reg_address.get('zipCode', ''))
+    # add_field_to_record('424', _generate_region_city(reg_address))
+    # add_field_to_record('425', prozhiv_address.get('zipCode', ''))
+    # add_field_to_record('426', _generate_region_city(prozhiv_address))
+    # add_field_to_record('427', foreigner)
+    add_field_to_record('501', '3')
+    add_field_to_record('510', datetime.now().strftime('%Y%m%d'))
+
+    # Trusted
+    # add_field_to_record('502', '1')
+    # add_field_to_record('503', '1')
+
+    return record
 
 
 def _create_or_update_ruslan_user(grs_user_record):
@@ -103,15 +212,30 @@ def _create_or_update_ruslan_user(grs_user_record):
     else:
         field_100 = fields_100[0]
 
-    field_100.content = RUSLAN_ID_MASK[:len(record_id) * -1] + record_id
-
     fields_115 = grs_user_record.get_field('115')
     if fields_115:
         exist_fields_115 = record.get_field('115')
         if not exist_fields_115:
             record.add_field(fields_115[0])
 
-    _update_ruslan_user(grs_user_record)
+    field_100.content = RUSLAN_ID_MASK[:len(record_id) * -1] + record_id
+    portal_client.update_grs(grs_record=record, database=RUSLAN_USERS_DATABASE, id=record_id)
+    return record
+
+
+def _update_ruslan_user(record):
+    portal_client = connection_pool.get_client(RUSLAN_API_ADDRESS, RUSLAN_API_USERNAME, RUSLAN_API_PASSWORD)
+
+    fields_1 = record.get_field('1')
+    record_id = ''
+
+    if fields_1:
+        record_id = fields_1[0].content
+
+    if not record_id:
+        raise ValueError('record_id must be not empty')
+
+    portal_client.update_grs(grs_record=record, database=RUSLAN_USERS_DATABASE, id=record_id)
     return record
 
 
@@ -166,6 +290,10 @@ def redirect_from_idp(request):
 
     try:
         person_info_resp = _get_person_info(access_token)
+        # print person_info_resp
+        # person_contacts = _get_person_contacts(oid, access_token)
+        # person_addresses = _get_person_addresses(oid, access_token)
+        # person_docs = _get_person_docs(oid, access_token)
     except Exception as e:
         return _error_response(
             request=request,
@@ -207,6 +335,12 @@ def redirect_from_idp(request):
     }
 
     portal_client = connection_pool.get_client(RUSLAN_API_ADDRESS, RUSLAN_API_USERNAME, RUSLAN_API_PASSWORD)
+    # oid = u'erf'
+    # state = '112313'
+    # user_attrs = esia_response
+    # person_info = user_attrs.get('person_info', {})
+    # person_contacts = user_attrs.get('person_contacts', [])
+
 
     sru_response = portal_client.search(
         query=u'@attrset bib-1 @attr 1=%s "%s"' % (
@@ -216,21 +350,19 @@ def redirect_from_idp(request):
     )
 
     sru_records = humanize.get_records(sru_response)
-
+    tatedu_user = models.create_or_update_user(user_id, user_attrs)
     if not sru_records:
-        tatedu_user = models.create_or_update_user(user_id, user_attrs)
         return register_new_user(request, tatedu_user.id)
     else:
-        exists_user_grs_record = grs.Record.from_dict(humanize.get_record_content(sru_records[0]))
-        tatedu_user = models.create_or_update_user(user_id, user_attrs)
-        user_grs_record = _create_grs_from_user(
+        user_grs_record = grs.Record.from_dict(humanize.get_record_content(sru_records[0]))
+        fields_100 = user_grs_record.get_field('100')
+        new_user_grs_record = _create_grs_from_user(
             oid=tatedu_user.oid,
             email=person_info.get('email', ''),
             user_attrs=user_attrs
         )
-        exists_user_grs_record.update(user_grs_record)
-        _update_ruslan_user(exists_user_grs_record)
-        fields_100 = user_grs_record.get_field('100')
+        user_grs_record.update(new_user_grs_record)
+        _update_ruslan_user(user_grs_record)
         if not fields_100:
             return _error_response(
                 request=request,
@@ -266,6 +398,7 @@ def redirect_from_idp(request):
                 # return HttpResponse(json.dumps(person_info, ensure_ascii=False), content_type='application/json')
 
 
+@transaction.atomic()
 def register_new_user(request, id):
     try:
         tatedu_user = models.TatEduUser.objects.get(id=id)
@@ -284,33 +417,24 @@ def register_new_user(request, id):
     reader_id = ''
     password = ''
 
-    user_grs_record = _create_grs_from_user(
-        oid=tatedu_user.oid,
-        email=person_info.get('email', ''),
-        user_attrs=user_attrs
-    )
-
     if not sru_records:
+        user_grs_record = _create_grs_from_user(
+            oid=tatedu_user.oid,
+            email=person_info.get('email', ''),
+            user_attrs=user_attrs
+        )
         fields_115 = user_grs_record.get_field('115')
         if fields_115:
             password = fields_115[0].content
-        exists_user_grs_record = _create_or_update_ruslan_user(user_grs_record)
-    else:
-        exists_user_grs_record = grs.Record.from_dict(humanize.get_record_content(sru_records[0]))
-        exists_user_grs_record.update(user_grs_record)
-        _update_ruslan_user(exists_user_grs_record)
+        user_grs_record = _create_or_update_ruslan_user(user_grs_record)
 
-    fields_100 = exists_user_grs_record.get_field('100')
+    else:
+        user_grs_record = grs.Record.from_dict(humanize.get_record_content(sru_records[0]))
+
+    fields_100 = user_grs_record.get_field('100')
 
     if fields_100:
         reader_id = fields_100[0].content
-    else:
-        return _error_response(
-            request=request,
-            error='no_user',
-            state='',
-            error_description=u'Не найден идентификатор пользователя в GRS записи'
-        )
 
     # tatedu_user.delete()
 
@@ -339,6 +463,91 @@ def register_new_user(request, id):
             state='',
             error_description=u'Система не может сопоставить вашу учетную запись ОЭ РТ'
         )
+
+
+#
+# @transaction.atomic()
+# def ask_for_exist_reader(request, id):
+#     try:
+#         esia_user = models.EsiaUser.objects.get(id=id)
+#     except models.EsiaUser.DoesNotExist:
+#         return redirect('sso_esia:index')
+#
+#     portal_client = connection_pool.get_client(RUSLAN_API_ADDRESS, RUSLAN_API_USERNAME, RUSLAN_API_PASSWORD)
+#
+#     if request.method == 'POST':
+#         ruslan_auth_form = forms.RuslanAuthForm(request.POST)
+#         if ruslan_auth_form.is_valid():
+#             reader_id = ruslan_auth_form.cleaned_data['reader_id'].replace('\\', '\\\\').replace('"', '\\"')
+#             password = ruslan_auth_form.cleaned_data['password'].replace('\\', '\\\\').replace('"', '\\"')
+#
+#             sru_response = portal_client.search(
+#                 query='@attrset bib-1 @attr 1=100 "%s"' % (reader_id,),
+#                 database=RUSLAN_USERS_DATABASE,
+#                 maximum_records=1
+#             )
+#
+#             sru_records = humanize.get_records(sru_response)
+#
+#             if not sru_records:
+#                 ruslan_auth_form.add_error('reader_id', u'Идентификатор читателя не найден')
+#             else:
+#                 sru_response = portal_client.search(
+#                     query='@attrset bib-1 @and @attr 1=100 "%s" @attr 1=115 "%s"' % (reader_id, password),
+#                     database=RUSLAN_USERS_DATABASE,
+#                     maximum_records=1
+#                 )
+#                 sru_records = humanize.get_records(sru_response)
+#                 if not sru_records:
+#                     ruslan_auth_form.add_error('reader_id', u'Неверный пароль')
+#                 else:
+#                     user_record = humanize.get_record_content(sru_records[0])
+#                     user_grs_record = grs.Record.from_dict(user_record)
+#                     fields_403 = user_grs_record.get_field('403')
+#
+#                     if fields_403:
+#                         if fields_403[0].content != esia_user.oid:
+#                             ruslan_auth_form.add_error(
+#                                 'reader_id',
+#                                 u'Идентификатор читателя уже связан с учетной записью ЭО РТ'
+#                             )
+#                     else:
+#                         user_grs_record.add_field(grs.Field('403', esia_user.oid))
+#                         portal_client.update_grs(
+#                             grs_record=user_grs_record,
+#                             database=RUSLAN_USERS_DATABASE,
+#                             id=reader_id
+#                         )
+#                         esia_user.delete()
+#                         user = authenticate(
+#                             username=ruslan_auth_form.cleaned_data['reader_id'],
+#                             password=ruslan_auth_form.cleaned_data['password']
+#                         )
+#                         if user:
+#                             if user.is_active:
+#                                 login(request, user)
+#                                 return redirect('index:frontend:index')
+#                             else:
+#                                 return _error_response(
+#                                     request=request,
+#                                     error='no_access_toke',
+#                                     state='',
+#                                     error_description=u'Ваша учетная запись читателя не активна'
+#                                 )
+#                         else:
+#                             return _error_response(
+#                                 request=request,
+#                                 error='no_user',
+#                                 state='',
+#                                 error_description=u'Система не может сопоставить вашу учетную запись ЭО РТ'
+#                             )
+#     else:
+#         ruslan_auth_form = forms.RuslanAuthForm()
+#
+#     return render(request, 'esia_sso/ask_for_exist_reader.html', {
+#         'ruslan_auth_form': ruslan_auth_form,
+#         'esia_id': id
+#     })
 
 
 def _get_person_info(access_token):
@@ -402,46 +611,3 @@ def _get_library_id(user_grs_record):
         except Library.DoesNotExist:
             pass
     return ''
-
-
-def _create_grs_from_user(oid, email='', user_attrs=None):
-    now = datetime.now()
-    user_attrs = user_attrs or {}
-    gender_map = {
-        'male': u'М',
-        'female': u'Ж'
-    }
-
-    person_info = user_attrs.get('person_info', {})
-    birth_date = person_info.get('birth_year', '')
-
-    if birth_date:
-        birth_date += '0101'
-
-    record = grs.Record()
-
-    def add_field_to_record(tag, value):
-        if not value:
-            return
-        record.add_field(grs.Field(tag, value))
-
-    add_field_to_record('101', normalize_fio(person_info.get('lname', '')))
-    add_field_to_record('102', normalize_fio(person_info.get('fname', '')))
-    add_field_to_record('103', normalize_fio(person_info.get('pname', '')))
-    add_field_to_record('105', now.strftime('%Y%m%d'))
-    add_field_to_record('115', _generate_password())
-    add_field_to_record('122', email)
-    add_field_to_record('234', birth_date)
-    add_field_to_record(OID_FIELD, oid)
-    add_field_to_record('404', gender_map.get(person_info.get('gender', '').lower(), u''))
-
-    organisations = person_info.get('organizations', [])
-
-    for organisation in organisations:
-        add_field_to_record('505', unicode(organisation.get('id', '')))
-        add_field_to_record('506', unicode(organisation.get('region', {}).get('id')))
-
-    add_field_to_record('501', '3')
-    add_field_to_record('510', now.strftime('%Y%m%d'))
-
-    return record
