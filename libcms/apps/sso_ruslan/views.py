66b414840aeff459009344641cafa2e307fce478
Sergey Dostovalov <dostovalov@gmail.com>
2018-12-05 18:40:14 +0300

    исправлен гео поиск

--- libcms/apps/sso_ruslan/views.py
+++ libcms/apps/sso_ruslan/views.py
@@ -1,4 +1,12 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from . import forms
+from . import models
 
-
+@login_required
 def change_email(request):
-    pass
+    form = forms.ChangeEmailForm()
+    ruslan_user = models.get_ruslan_user(request)
+    return render(request, 'sso_ruslan/change_email.html', {
+        'form': form,
+    })
