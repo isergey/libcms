abff38c8150357b91d197110d36d7cdac2192b4d
Sergey Dostovalov <dostovalov@gmail.com>
2018-12-06 22:55:30 +0300

    сервисы для аутентификации

--- libcms/apps/accounts/api/urls.py
+++ libcms/apps/accounts/api/urls.py
@@ -4,4 +4,6 @@ import views
 
 urlpatterns = patterns(views,
     url(r'^user/$', views.user, name="user"),
+    url(r'^get_user/$', views.get_user, name="get_user"),
+    url(r'^check_password/$', views.check_password, name="check_password"),
 )
