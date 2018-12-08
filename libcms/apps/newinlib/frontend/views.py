3caa36ef20663fac5374a422eeb7b1def9c6ca32
Sergey Dostovalov <dostovalov@gmail.com>
2018-12-08 20:55:15 +0300

    upd

--- libcms/apps/newinlib/frontend/views.py
+++ libcms/apps/newinlib/frontend/views.py
@@ -10,40 +10,12 @@ from ..models import Item, ItemContent
 
 def index(request):
     cur_language = translation.get_language()
-    # items_page = get_page(request, Item.objects.filter(publicated=True).order_by('-create_date'), per_page=10)
-    # item_contents = list(ItemContent.objects.select_related('item').filter(item__in=list(items_page.object_list), lang=cur_language))
-    #
-    # t_dict = {}
-    # for item in items_page.object_list:
-    #     t_dict[item.id] = {
-    #         'item': item
-    #     }
-    #
-    # for item_content in item_contents:
-    #     t_dict[item_content.item_id]['item'].item_content = item_content
-    #
-    # nd = {}
-    #
-    # for item in item_contents:
-    #     nd[item.item.id_in_catalog] = item
-    #
-    # records_ids = []
-    #
-    # for items_lis in item_contents:
-    #     records_ids.append(items_lis.item.id_in_catalog)
-    #
-    # records = get_records(records_ids)
-    # for record in records:
-    #     doc = utils.xml_doc_to_dict(record.content)
-    #     item = nd.get(record.gen_id)
-    #     print(item)
-    #     if item is not None:
-    #         item.item.cover = doc.get('cover', [''])[0]
 
     items_page = get_page(
         request,
         ItemContent.objects.prefetch_related('item')
-            .filter(item__publicated=True, lang=cur_language).order_by('-item__create_date')
+            .filter(item__publicated=True, lang=cur_language)
+            .order_by('-item__create_date')
     )
 
     nd = {}
