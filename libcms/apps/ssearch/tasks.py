3bdf97db7865cafc04110812fe2442d799cd77b7
Sergey Dostovalov <dostovalov@gmail.com>
2019-01-29 20:57:17 +0300

    асинхронное индексирование

--- libcms/apps/ssearch/tasks.py
+++ libcms/apps/ssearch/tasks.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from huey.contrib.djhuey import db_task, lock_task
 from . indexing import _indexing
 
 
 @db_task()
-@lock_task('ssearch.indexing')
+@settings.HUEY.lock_task('ssearch.indexing')
 def indexing(slug, reset):
     print 'start indexing', slug, 'reset', reset
     _indexing(slug, reset=reset)
