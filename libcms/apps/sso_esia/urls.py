a04e0fda90b3572592ee0a9817e2940b1f9d6bb8
Sergey Dostovalov <dostovalov@gmail.com>
2019-04-16 16:57:25 +0300

    есиа

--- libcms/apps/sso_esia/urls.py
+++ libcms/apps/sso_esia/urls.py
@@ -4,7 +4,7 @@ import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name="index"),
-    url(r'^redirect$', views.redirect_from_idp, name="redirect_from_ip"),
+    url(r'^redirect/$', views.redirect_from_idp, name="redirect_from_ip"),
     url(r'^ask/(?P<id>\d+)$', views.ask_for_exist_reader, name="ask_for_exist_reader"),
     url(r'^register/(?P<id>\d+)$', views.register_new_user, name="register_new_user"),
     # url(r'^grs$', views.grs_test, name="create_or_update_ruslan_user"),
